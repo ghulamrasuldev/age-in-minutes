a0252e61d9da2477cf94197d42f92ddecb771ca6
ghulamrasuldev <ghulam.rasool.development@gmail.com>
2021-09-25 17:15:53 +0500

    added calender

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -1,25 +1,31 @@
 package com.example.ageinminutes
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.CalendarView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
-
-
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.btnDatePicker)
-        button.setOnClickListener{
-            val simpleCalendarView =
-                findViewById<View>(R.id.simpleCalendarView) as CalendarView // get the reference of CalendarView
-            val selectedDate = simpleCalendarView.date
-
+        val btnDatePicker = findViewById<Button>(R.id.btnDatePicker)
+        btnDatePicker.setOnClickListener{ view ->
+            clickDatePicker(view)
+            Toast.makeText(this, "ButtonWorks!", Toast.LENGTH_LONG).show()
         }
+    }
+    fun clickDatePicker(view: View) {
+        val myCalender = Calendar.getInstance()
+        val year = myCalender.get(Calendar.YEAR)
+        val month = myCalender.get(Calendar.MONTH)
+        val day = myCalender.get(Calendar.DAY_OF_MONTH )
+        DatePickerDialog(this, DatePickerDialog.OnDateSetListener{
+                view, year, month, dayOfMonth ->
+        }, year, month, day).show()
     }
 }
