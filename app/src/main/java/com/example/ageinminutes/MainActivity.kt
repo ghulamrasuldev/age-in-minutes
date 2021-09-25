2f2ebd0de1c1c2d7e8ef4d4de5b1c8ef27adec99
ghulamrasuldev <ghulam.rasool.development@gmail.com>
2021-09-25 16:44:11 +0500

    last update

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.ageinminutes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CalendarView
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.btnDatePicker)
+        button.setOnClickListener{
+            val simpleCalendarView =
+                findViewById<View>(R.id.simpleCalendarView) as CalendarView // get the reference of CalendarView
+            val selectedDate = simpleCalendarView.date
 
+        }
     }
 }
